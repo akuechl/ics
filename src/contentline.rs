fd93175728916910f54a8050b7381ce922e23cd4
Ariel Küchler <4943837+akuechl@users.noreply.github.com>
2024-09-24 22:33:37 +0200

    Fix fold, limit 74

--- src/contentline.rs
+++ src/contentline.rs
@@ -8,13 +8,13 @@ pub const LIMIT: usize = 75;
 const LINE_BREAK: &str = "\r\n ";
 
 pub fn fold<W: fmt::Write>(writer: &mut W, mut content: &str) -> fmt::Result {
-    let mut boundary = next_boundary(content);
+    let mut boundary = next_boundary(content, LIMIT);
     writer.write_str(&content[..boundary])?;
 
     while boundary < content.len() {
         content = &content[boundary..];
         writer.write_str(LINE_BREAK)?;
-        let next_boundary = next_boundary(content);
+        let next_boundary = next_boundary(content, LIMIT - 1);
         writer.write_str(&content[..next_boundary])?;
         boundary = next_boundary;
     }
@@ -23,12 +23,12 @@ pub fn fold<W: fmt::Write>(writer: &mut W, mut content: &str) -> fmt::Result {
 
 // TODO: unfold algorithm
 
-fn next_boundary(input: &str) -> usize {
+fn next_boundary(input: &str, limit: usize) -> usize {
     let input = input.as_bytes();
-    if LIMIT >= input.len() {
+    if limit >= input.len() {
         return input.len();
     }
-    match input[..=LIMIT].iter().rposition(|&i| i < 128 || i >= 192) {
+    match input[..=limit].iter().rposition(|&i| i < 128 || i >= 192) {
         Some(0) | None => input.len(),
         Some(index) => index,
     }
@@ -36,11 +36,7 @@ fn next_boundary(input: &str) -> usize {
 
 // Calculates the new estimated text length after inserting line breaks
 pub fn size(len: usize) -> usize {
-    if len % LIMIT == 0 {
-        len + (len / LIMIT - 1) * 3
-    } else {
-        len + (len / LIMIT) * 3
-    }
+    len + ((len - 2) / (LIMIT - 1)) * 3
 }
 
 #[cfg(test)]
@@ -94,8 +90,27 @@ mod tests {
         let content = "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy cog. The quick brown fox jumps over the lazy hog. The quick brown fox jumps over the lazy log. The quick brown fox jumps over the lazy dog. ";
         let mut line = String::with_capacity(size(content.len()));
         fold(&mut line, content).unwrap();
-        let expected = "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over\r\n  the lazy cog. The quick brown fox jumps over the lazy hog. The quick brown\r\n  fox jumps over the lazy log. The quick brown fox jumps over the lazy dog. ";
+        let expected = "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over\r\n  the lazy cog. The quick brown fox jumps over the lazy hog. The quick brow\r\n n fox jumps over the lazy log. The quick brown fox jumps over the lazy dog\r\n . ";
 
         assert_eq!(line, expected);
+    }
+
+    #[test]
+    fn sizes() {
+        assert_eq!(12 + 0 * 3, size(12));
+        assert_eq!(75 + 0 * 3, size(75));
+        assert_eq!(76 + 1 * 3, size(76));
+
+        assert_eq!(148 + 1 * 3, size(148));
+        assert_eq!(149 + 1 * 3, size(149));
+        assert_eq!(150 + 2 * 3, size(150));
+        
+        assert_eq!(222 + 2 * 3, size(222));
+        assert_eq!(223 + 2 * 3, size(223));
+        assert_eq!(224 + 3 * 3, size(224));
+
+        assert_eq!(296 + 3 * 3, size(296));
+        assert_eq!(297 + 3 * 3, size(297));
+        assert_eq!(298 + 4 * 3, size(298));
     }
 }
